8805f6bb05082bdaad66380656f981c070963315
pawprintsapp <kayyemd@gmail.com>
2026-01-12 16:40:43 -0700

    feat: initial ios autocomplete module

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,111 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-ios-autocomplete';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-const result = multiply(3, 7);
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import AddressAutocomplete from 'react-native-ios-autocomplete';
+import { getCurrentLocation, getLocationPermissions } from './LocationHelper';
 
 export default function App() {
+  const [enteredAddress, setEnteredAddress] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  useEffect(() => {
+    const reverseGeocode = async () => {
+      await getLocationPermissions();
+
+      const location = await getCurrentLocation();
+      console.log(location);
+
+      const addressDetails = await AddressAutocomplete.reverseGeocodeLocation(
+        location.coords.longitude,
+        location.coords.latitude
+      );
+      console.log(addressDetails);
+    };
+    reverseGeocode();
+  }, []);
+
+  const onChange = async (text: string) => {
+    setEnteredAddress(text);
+    AddressAutocomplete.getAddressSuggestions(text)
+      .then((addresses) => {
+        setSuggestions(addresses);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder="Address"
+            value={enteredAddress}
+            onChangeText={onChange}
+          />
+        </View>
+
+        <View style={styles.suggestionsContainer}>
+          <ScrollView style={styles.suggestions}>
+            {suggestions.map((address, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={async () => {
+                    try {
+                      const addressDetails =
+                        await AddressAutocomplete.getAddressDetails(address);
+                      console.log(addressDetails.title);
+                    } catch (e) {
+                      console.log(e);
+                    }
+                  }}
+                >
+                  <View style={styles.address}>
+                    <Text>{address}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    borderWidth: 1,
+    borderColor: 'red',
+    height: 40,
+    padding: 5,
   },
+  inputWrapper: {
+    marginVertical: 15,
+  },
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+  address: {
+    paddingVertical: 10,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+  suggestions: {},
+  suggestionsContainer: {},
 });
